0d0f6d7cf97b8ee5f9fb5232588d0a091d13d05d
yazeerr <int.yazeer@gmail.com>
2021-11-30 15:22:35 +0530

    added custom error

--- middleware/error-handler.ts
+++ middleware/error-handler.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import {CustomAPIError} from '../errors/custom-error'
 
 
 export const errorHandlerMiddleware = async(err: Error, req: Request, res: Response, next:NextFunction):Promise<Response> => {
-    console.error(err)
+    
+    if (err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({msg: err.message})
+    }
     return res.status(500).json('Something went wrong. Please try again later!')
 }
