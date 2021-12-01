21c1f817190961f87039821396276280020475e8
yazeerr <int.yazeer@gmail.com>
2021-12-02 00:09:37 +0530

    Added status codes

--- middleware/error-handler.ts
+++ middleware/error-handler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import {CustomAPIError} from '../errors/custom-error'
+import {StatusCodes} from 'http-status-codes'
 
 
 export const errorHandlerMiddleware = async(err: Error, req: Request, res: Response, next:NextFunction):Promise<Response> => {
@@ -7,5 +8,5 @@ export const errorHandlerMiddleware = async(err: Error, req: Request, res: Respo
     if (err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({msg: err.message})
     }
-    return res.status(500).json('Something went wrong. Please try again later!')
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json('Something went wrong. Please try again later!')
 }
--- middleware/not-found.ts
+++ middleware/not-found.ts
@@ -1,3 +1,4 @@
 import { Response, Request } from "express";
+import {StatusCodes} from 'http-status-codes'
 
-export const notFound = (req:Request, res:Response) => res.status(404).json('Route does not exist')
+export const notFound = (req:Request, res:Response) => res.status(StatusCodes.NOT_FOUND).json('Route does not exist')
